48492637f3006bfe220d18f6cf9757b5982a92f4
Tatiana <marmurka.rift@gmail.com>
2020-05-01 11:50:45 +0200

    This is my first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pl = 3.14;
+
+console.log(leftBorderWidth);
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone[name]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[2]);
