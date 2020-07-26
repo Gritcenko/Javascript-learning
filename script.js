42d187003e4ee7b05ef31e85daacb9a48605baa2
Gritcenko <marmurka.rift@gmail.com>
2020-07-26 18:11:19 +0300

    Practice, part 1

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var leftBorderWidth = 1;
-let second = 2;
-const pl = 3.14;
+const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?',' ');
 
-console.log(leftBorderWidth);
-
-
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
 };
+const oneMovie = prompt('Один из просмотренных фильмов?', ' '),
+    rating = prompt('На сколько оцените его?',' '),
+    oneMovie2 = prompt('Один из просмотренных фильмов?', ' '),
+    rating2 = prompt('На сколько оцените его?',' ');
 
-console.log(persone[name]);
+personalMovieDB.movies[oneMovie] = rating;
+personalMovieDB.movies[oneMovie2] = rating2;
 
-let arr = ['plum.png','orange.jpg','apple.bmp'];
-
-console.log(arr[2]);
+console.log(personalMovieDB);
